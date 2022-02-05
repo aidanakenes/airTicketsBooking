009d40fc7aa74a8db186a888672cdc0c0c26f8a6
aidanakenes <aidanaknsm@gmail.com>
2022-02-05 20:33:03 +0600

    add: carcas

---
+++ code/app.py
@@ -0,0 +1,12 @@
+from sanic import Sanic, response
+
+app = Sanic('sample-web-service')
+
+
+@app.route('/')
+async def test(request):
+    return response.json({'test': True})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
