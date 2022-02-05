40eaeb408f0d4d3f08efcdf37b6b81f683d61e7d
aidanakenes <aidanaknsm@gmail.com>
2022-02-05 23:19:52 +0600

    add: routing for search() handler

--- code/app.py
+++ code/app.py
@@ -1,9 +1,13 @@
 from sanic import Sanic, response
 
+from handlers import search
+
 app = Sanic('air-tickets-booking')
 
 
 def run():
+    app.add_route(search.search, "/search")
+
     app.run(host='0.0.0.0', port=8000)
 
 
