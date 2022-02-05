a2ce7054a48aa668c615b2e7088f523ec5f318a5
aidanakenes <aidanaknsm@gmail.com>
2022-02-05 23:54:23 +0600

    fix: correct sample data

--- code/handlers/booking.py
+++ code/handlers/booking.py
@@ -4,25 +4,8 @@ from sanic import Sanic, response
 async def booking(request):
 
     sample_response = {
-      "offer_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      "phone": "+77777777777",
-      "email": "user@example.com",
-      "passengers": [
-        {
-          "gender": "M",
-          "type": "ADT",
-          "first_name": "CRAIGE",
-          "last_name": "BENSEN",
-          "date_of_birth": "1987-01-24",
-          "citizenship": "US",
-          "document": {
-            "number": "N234346356",
-            "expires_at": "2025-01-24",
-            "iin": "123456789123"
-          }
+         "id": "d9e0cf5a-6bb8-4dae-8411-6caddcfd52da"
         }
-      ]
-    }
 
     return response.json({'test': sample_response})
 
