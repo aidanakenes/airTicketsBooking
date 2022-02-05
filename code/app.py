0083e4a0feecf3499a1104a48f0b6f2784f8343d
aidanakenes <aidanaknsm@gmail.com>
2022-02-05 23:55:30 +0600

    add: params type & http methods type

--- code/app.py
+++ code/app.py
@@ -6,14 +6,14 @@ app = Sanic('air-tickets-booking')
 
 
 def run():
-    app.add_route(search.search, "/search")
-    app.add_route(search.search_by_id, "/search/<search_id>")
+    app.add_route(search.search, "/search", methods=["POST"])
+    app.add_route(search.search_by_id, "/search/<search_id:int>", methods=["GET"])
 
-    app.add_route(offers.offers_by_id, "/offers/<offer_id>")
+    app.add_route(offers.offers_by_id, "/offers/<offer_id:int>", methods=["GET"])
 
-    app.add_route(booking.booking, "/booking")
-    app.add_route(booking.booking_by_id, "/booking/<booking_id>")
-    app.add_route(booking.booking_by_email_phone, "/booking/<email>/<phone>")
+    app.add_route(booking.booking, "/booking", methods=["POST"])
+    app.add_route(booking.booking_by_id, "/booking/<booking_id:int>", methods=["GET"])
+    app.add_route(booking.booking_by_email_phone, "/booking/<email:string>/<phone:[+,0-9]{0,4}>", methods=["GET"])
 
     app.run(host='0.0.0.0', port=8000)
 
