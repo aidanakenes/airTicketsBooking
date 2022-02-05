72e58d2ffe906eefc6a75c9d689272c3e68e9fbd
aidanakenes <aidanaknsm@gmail.com>
2022-02-05 23:41:59 +0600

    fix: correct handlers and imports

--- code/app.py
+++ code/app.py
@@ -1,6 +1,6 @@
 from sanic import Sanic, response
 
-from handlers import search
+from handlers import search, offers
 
 app = Sanic('air-tickets-booking')
 
@@ -9,7 +9,7 @@ def run():
     app.add_route(search.search, "/search")
     app.add_route(search.search_by_id, "/search/<search_id>")
 
-    app.add_route(search.search_by_id, "/offers/<offer_id>")
+    app.add_route(offers.offers_by_id, "/offers/<offer_id>")
 
     app.run(host='0.0.0.0', port=8000)
 
