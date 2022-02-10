d03f72cf4467f925ccc0d197994147f83735cfd9
aidanakenes <aidanaknsm@gmail.com>
2022-02-10 11:25:48 +0600

    style: rename from sample_result to search_results

--- code/handlers/search.py
+++ code/handlers/search.py
@@ -3,11 +3,11 @@ from sanic import Sanic, response
 
 async def search(request):
 
-    sample_result = {
+    search_results = {
         "id": "d9e0cf5a-6bb8-4dae-8411-6caddcfd52da"
     }
 
-    return response.json(sample_result)
+    return response.json(search_results)
 
 
 async def search_by_id(request, search_id):
