424ee63c9972161074f435d3639e0bb51619fc4b
aidanakenes <aidanaknsm@gmail.com>
2022-02-10 11:31:08 +0600

    fix: correct response for offer_details handler

--- code/handlers/offers.py
+++ code/handlers/offers.py
@@ -2,4 +2,84 @@ from sanic import Sanic, response
 
 
 async def offer_details(request, offer_id):
-    return response.json({'test': offer_id})
+
+    offer_details_result = {
+      "id": "3c0d66ca-47e2-4e4e-9c9f-21b774b64a7f",
+      "flights": [
+        {
+          "duration": 18000,
+          "segments": [
+            {
+              "operating_airline": "DV",
+              "flight_number": "828",
+              "equipment": "Airbus A320-100/200",
+              "cabin": "Economy",
+              "dep": {
+                "at": "2022-02-09T03:05:00+06:00",
+                "airport": {
+                  "code": "ALA",
+                  "name": "Алматы"
+                },
+                "terminal": "4"
+              },
+              "arr": {
+                "at": "2022-02-09T05:55:00+06:00",
+                "airport": {
+                  "code": "CIT",
+                  "name": "Шымкент"
+                },
+                "terminal": "1"
+              },
+              "baggage": "1PC"
+            },
+            {
+              "operating_airline": "DV",
+              "flight_number": "958",
+              "equipment": "Boeing 767-200",
+              "cabin": "Economy",
+              "dep": {
+                "at": "2022-02-09T09:15:00+06:00",
+                "airport": {
+                  "code": "CIT",
+                  "name": "Шымкент"
+                },
+                "terminal": "3"
+              },
+              "arr": {
+                "at": "2022-02-09T11:25:00+06:00",
+                "airport": {
+                  "code": "NQZ",
+                  "name": "Нур-Султан (Астана)"
+                },
+                "terminal": "4"
+              },
+              "baggage": "1PC"
+            }
+          ]
+        }
+      ],
+      "price": {
+        "amount": 87736,
+        "currency": "KZT"
+      },
+      "refundable": True,
+      "baggage": "1PC",
+      "cabin": "Economy",
+      "type": "OW",
+      "airline": {
+        "code": "DV",
+        "name": "SCAT",
+        "logo": {
+          "url": "http://localhost/img/5661-501f546c73c976a96cf0d18e600b4d7a.gif",
+          "width": 1416,
+          "height": 274
+        }
+      },
+      "passengers": {
+        "ADT": 1,
+        "CHD": 0,
+        "INF": 0
+      }
+    }
+
+    return response.json(offer_details_result)
