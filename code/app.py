574afa0738e249096ff0cf6ce3a561036e86d4fc
aidanakenes <aidanaknsm@gmail.com>
2022-02-10 11:32:50 +0600

    refact: change id type from int to uuid

--- code/app.py
+++ code/app.py
@@ -7,12 +7,12 @@ app = Sanic('air-tickets-booking')
 
 def run():
     app.add_route(search.search, "/search", methods=["POST"])
-    app.add_route(search.search_by_id, "/search/<search_id:int>", methods=["GET"])
+    app.add_route(search.search_by_id, "/search/<search_id:uuid>", methods=["GET"])
 
-    app.add_route(offers.offer_details, "/offers/<offer_id:int>", methods=["GET"])
+    app.add_route(offers.offer_details, "/offers/<offer_id:uuid>", methods=["GET"])
 
     app.add_route(booking.booking, "/booking", methods=["POST"])
-    app.add_route(booking.booking_by_id, "/booking/<booking_id:int>", methods=["GET"])
+    app.add_route(booking.booking_by_id, "/booking/<booking_id:uuid>", methods=["GET"])
     app.add_route(booking.booking_by_email_phone, "/booking/<email:string>/<phone:[+,0-9]{0,4}>", methods=["GET"])
 
     app.run(host='0.0.0.0', port=8000)
