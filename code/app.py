7621e04369952ca033355f454c3ef604a128dda9
aidanakenes <aidanaknsm@gmail.com>
2022-02-20 19:12:56 +0600

    refact: remove decorator and changes args type

--- code/app.py
+++ code/app.py
@@ -7,19 +7,20 @@ import settings
 app = Sanic('air-tickets-booking')
 
 
-@app.listener("before_server_start")
 async def init_before(app, loop):
     app.ctx.db_pool = await asyncpg.create_pool(dsn=settings.DATABASE_URL, loop=loop)
 
 
 def run():
+    app.register_listener(init_before, "before_server_start")
+
     app.add_route(search.search, "/search", methods=["POST"])
     app.add_route(search.search_by_id, "/search/<search_id:uuid>", methods=["GET"])
 
     app.add_route(offers.offer_details, "/offers/<offer_id:uuid>", methods=["GET"])
 
     app.add_route(booking.create_booking, "/booking", methods=["POST"])
-    app.add_route(booking.booking_details, "/booking/<booking_id:uuid>", methods=["GET"])
+    app.add_route(booking.booking_details, "/booking/<booking_id:str>", methods=["GET"])
     app.add_route(booking.get_bookings, "/booking", methods=["GET"])
 
     app.run(host='0.0.0.0', port=8000)
