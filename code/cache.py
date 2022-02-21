5c7b64c61a473d595b8f6ccfce18dccfa8b370e9
aidanakenes <aidanaknsm@gmail.com>
2022-02-21 19:50:00 +0600

    refact: separate cache methods for search & offer

--- code/cache.py
+++ code/cache.py
@@ -1,41 +1,29 @@
 import json
 
-
-async def cache_search(request, search_id):
-    await request.app.redis.setex(f'internship:{search_id}', settings.REDIS_TTL, request.json)
+import settings
 
 
-async def get_search_cache(request, search_id):
-    data = await request.app.ctx.redis.get(f'internship:{search_id}')
-    return json.loads(data)
+async def save_search(redis, search_data, search_id):
+    await redis.setex(f'internship:{search_id}',
+                      settings.REDIS_SEARCH_TTL,
+                      json.dumps(search_data))
 
 
-async def get_cache(request, offer_id):
-    data = await request.app.ctx.redis.get(f'internship:{offer_id}')
-    return json.loads(data)
+async def get_search(redis, search_id):
+    data = await redis.get(f'internship:{search_id}')
+
+    if data:
+        return json.loads(data)
 
 
 async def save_currency(redis, currency_details):
-    await redis.setex(f'currency', 86400, json.dumps(currency_details))
+    await redis.setex(f'currency',
+                      settings.REDIS_CURRENCY_TTL,
+                      json.dumps(currency_details))
 
 
 async def get_currency(redis):
     data = await redis.get(f'currency')
-    if data is None:
-        return
 
-    return json.loads(data)
-
-
-async def convert_currency(request, data):
-    cached_currency = await request.app.ctx.redis.get(f'currency')
-    currency_price = 1.0
-
-    for cur in json.loads(cached_currency).get('rates').get('item'):
-        currency_price = cur.get('description')
-
-    for r in data.get('items'):
-        r['price']['amount'], r['price']['currency'] = round(
-            float(r.get('price').get('amount')) / float(currency_price), 2), request.json.get('currency')
-
-    return data
+    if data:
+        return json.loads(data)
