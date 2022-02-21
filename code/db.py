dd66aaff1262369c61786a597eb6f069e0eb1906
aidanakenes <aidanaknsm@gmail.com>
2022-02-21 19:00:04 +0600

    refact: separate db methods for booking

--- code/db.py
+++ code/db.py
@@ -10,20 +10,43 @@ async def create_booking(booking, *args, **kwargs):
     conn = kwargs.pop('connection')
 
     for passenger in booking.passengers:
-        expires_at = datetime.strptime(passenger.date_of_birth, '%Y-%m-%d')
-        date_of_birth = datetime.strptime(passenger.date_of_birth, '%Y-%m-%d')
-        uid = await conn.fetchval("""insert into passenger (gender, ticket_type, first_name, last_name, date_of_birth, citizenship, numbers, expires_at, iin)
-                           values ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING passenger_id;""", passenger.gender,
-                                  passenger.ticket_type,
-                                  passenger.first_name,
-                                  passenger.last_name, date_of_birth, passenger.citizenship,
-                                  passenger.document_number, expires_at, passenger.document_iin)
+        passenger_uid = await _insert_passenger(conn, passenger)
+        offer_details_uid = await _insert_offer(conn, booking)
+        booking_id = await _insert_booking(conn, booking, passenger_uid, offer_details_uid)
 
-        details_uid = await conn.fetchval("""insert into offer_details (details)
-                           values ($1) RETURNING offer_details_id;""", json.dumps(booking.offer))
-        await conn.execute("""insert into booking (offer_id, phone, email, offer_details_id, passenger_id)
-                           values ($1, $2, $3, $4, $5);""", booking.booking_id, booking.phone, booking.email,
-                           details_uid, uid)
+    return booking_id
+
+
+async def _insert_passenger(conn, passenger):
+    expires_at = datetime.strptime(passenger.date_of_birth, '%Y-%m-%d')
+    date_of_birth = datetime.strptime(passenger.date_of_birth, '%Y-%m-%d')
+
+    stmt = """INSERT INTO passenger (gender, ticket_type, first_name, last_name, date_of_birth, citizenship, numbers, expires_at, iin)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
+            RETURNING passenger_id;"""
+
+    uid = await conn.fetchval(stmt, passenger.gender, passenger.ticket_type, passenger.first_name,
+                              passenger.last_name, date_of_birth, passenger.citizenship,
+                              passenger.document_number, expires_at, passenger.document_iin)
+
+    return uid
+
+
+async def _insert_offer(conn, booking):
+    stmt = """INSERT INTO offer_details (details)
+            VALUES ($1) RETURNING offer_details_id;"""
+    uid = await conn.fetchval(stmt, json.dumps(booking.offer))
+
+    return uid
+
+
+async def _insert_booking(conn, booking, passenger_id, offer_details_id):
+    stmt = """INSERT INTO booking (offer_id, phone, email, offer_details_id, passenger_id)
+            VALUES ($1, $2, $3, $4, $5) RETURNING offer_id;"""
+    uid = await conn.execute(stmt, booking.booking_id, booking.phone, booking.email,
+                             offer_details_id, passenger_id)
+
+    return uid
 
 
 @helpers.with_connection
@@ -53,7 +76,7 @@ inner join offer_details od on b.offer_details_id=od.offer_details_id where offe
         })
 
     data = {
-        'id': data.get('offer_id'),
+        'id': booking_id,
         'phone': data.get('phone'),
         'email': data.get('email'),
         'offer': json.loads(data.get('details')),
@@ -68,7 +91,8 @@ async def get_bookings(email, phone, *args, **kwargs):
     conn = kwargs.pop('connection')
 
     rows = await conn.fetch(
-        "select * from booking b inner join offer_details od on b.offer_details_id=od.offer_details_id where b.email=$1 and b.phone=$2;", email, phone)
+        "select * from booking b inner join offer_details od on b.offer_details_id=od.offer_details_id where b.email=$1 and b.phone=$2;",
+        email, phone)
     print()
     bookings = []
     for row in rows:
