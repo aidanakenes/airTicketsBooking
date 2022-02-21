8a2dc64c5387d7d70b190059720d9dc54df26ff6
aidanakenes <aidanaknsm@gmail.com>
2022-02-21 20:08:52 +0600

    refact: search handlers logic

--- code/handlers/search.py
+++ code/handlers/search.py
@@ -6,25 +6,28 @@ import json
 
 import schemas
 import cache
+from helpers.helpers import convert_currency
 
 
 async def search(request):
     jsonschema.validate(request.json, schema=schemas.SEARCH_SCHEMA)
 
     async with httpx.AsyncClient() as client:
-        resp = await client.post('https://avia-api.k8s-test.aviata.team/offers/search', json=request.json, timeout=30)
+        provider_response = await client.post('https://avia-api.k8s-test.aviata.team/offers/search',
+                                              json=request.json,
+                                              timeout=30)
         search_results = {
-            'search_id': resp.json().get('search_id')
+            'search_id': provider_response.json().get('search_id')
         }
 
-        data = await cache.convert_currency(request, resp.json())
+        data = await convert_currency(request, provider_response.json(), request.json.get('currency'))
 
-    await cache.cache_search(request, data)
+    await cache.save_search(request.app.ctx.redis, data, search_results.get('search_id'))
 
     return response.json(search_results)
 
 
 async def search_by_id(request, search_id):
-    search_results_by_id = await cache.get_search_cache(request, search_id)
+    search_results_by_id = await cache.get_search(request.app.ctx.redis, search_id)
 
     return response.json(search_results_by_id, dumps=json.dumps, default=str)
