8f6020331747d1f9e3957b84a3eecf1459cb83da
aidanakenes <aidanaknsm@gmail.com>
2022-02-22 12:47:31 +0600

    add: key generate for search cache

--- code/cache.py
+++ code/cache.py
@@ -3,16 +3,20 @@ import json
 import settings
 
 
+async def generate_search_key(search_id):
+    return f'inernship: {search_id}'
+
+
 async def save_search(redis, search_data, search_id):
     await redis.setex(
-        f'internship:{search_id}',
+        generate_search_key(search_id),
         settings.REDIS_SEARCH_TTL,
         json.dumps(search_data),
     )
 
 
 async def get_search(redis, search_id):
-    data = await redis.get(f'internship:{search_id}')
+    data = await redis.get(generate_search_key(search_id))
 
     if data:
         return json.loads(data)
