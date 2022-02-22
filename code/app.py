0583f430926366ac3c998330e35e86b9a20bf9fa
aidanakenes <aidanaknsm@gmail.com>
2022-02-22 17:16:38 +0600

    add: scheduler & retry decorator

--- code/app.py
+++ code/app.py
@@ -2,16 +2,20 @@ from sanic import Sanic, response
 import asyncpg
 import aioredis
 import httpx
+from httpx._exceptions import TimeoutException
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import traceback
 from datetime import datetime
 import xmltodict
 
 from handlers import search, offers, booking
+from helpers import decorators
 import cache
 import settings
 
 app = Sanic('air-tickets-booking')
+scheduler = AsyncIOScheduler()
 
 
 async def init_before(app, loop):
@@ -29,11 +33,16 @@ async def server_error_handler(request, error: Exception):
     return response.json({'error': str(error)}, status_code)
 
 
+@decorators.retry(exc_to_check=TimeoutException, tries=2, delay=2)
 async def currency_update(app, loop):
     if await cache.get_currency(app.ctx.redis) is None:
         async with httpx.AsyncClient() as client:
-            resp = await client.get('https://www.nationalbank.kz/rss/get_rates.cfm',
-                                     params={'fdate': datetime.today().strftime('%d.%m.%Y')}, timeout=30)
+            resp = await client.get(
+                'https://www.nationalbank.kz/rss/get_rates.cfm',
+                params={'fdate': datetime.today().strftime('%d.%m.%Y')},
+                timeout=30,
+            )
+
             data = xmltodict.parse(resp.text)
             await cache.save_currency(app.ctx.redis, data)
 
@@ -41,13 +50,22 @@ async def currency_update(app, loop):
 def run():
     app.register_listener(init_before, "before_server_start")
     app.register_listener(cleanup, "after_server_stop")
-    app.register_listener(currency_update, "before_server_start")
+    scheduler.add_job(
+        currency_update,
+        trigger='cron',
+        minute=59,
+        hour=23,
+        max_instances=1,
+        replace_existing=True
+    )
+
+    scheduler.start()
     app.error_handler.add(Exception, server_error_handler)
 
     app.add_route(search.search, "/search", methods=["POST"])
     app.add_route(search.search_by_id, "/search/<search_id:str>", methods=["GET"])
 
-    app.add_route(offers.offer_details, "/offers/<search_id:str>/<offer_id:str>", methods=["GET"])
+    app.add_route(offers.offer_details, "/offers/<offer_id:str>", methods=["GET"])
 
     app.add_route(booking.create_booking, "/booking", methods=["POST"])
     app.add_route(booking.booking_details, "/booking/<booking_id:str>", methods=["GET"])
--- code/helpers/decorators.py
+++ code/helpers/decorators.py
@@ -1,3 +1,7 @@
+import functools
+import time
+
+
 def with_connection(f):
     async def with_connection_(pool, *args, **kwargs):
 
@@ -16,3 +20,21 @@ def with_connection(f):
         return result
 
     return with_connection_
+
+
+def retry(exc_to_check, tries: int = 5, delay: int = 3):
+    def deco_retry(func):
+        @functools.wraps(func)
+        def func_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 0:
+                try:
+                    return func(*args, *kwargs)
+                except exc_to_check as e:
+                    time.sleep(mdelay)
+                    mtries -= 1
+            return func(*args, **kwargs)
+
+        return func_retry
+
+    return deco_retry
