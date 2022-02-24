517c87cf0b56c46b7cc571eaea3a3ee061127fd2
aidanakenes <aidanaknsm@gmail.com>
2022-02-24 22:47:13 +0600

    add: error if cache not found

--- code/handlers/offers.py
+++ code/handlers/offers.py
@@ -3,10 +3,14 @@ from sanic import Sanic, response
 import json
 
 import cache
+from helpers import errors
 
 
 async def offer_details(request, offer_id):
 
     offer_details_result = await cache.get_offer(request.app.ctx.redis, offer_id)
+
+    if offer_details_result is None:
+        raise errors.SearchNotFound()
 
     return response.json(offer_details_result, dumps=json.dumps, default=str)
