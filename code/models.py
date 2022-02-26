ce98349409c1713f32cbc22b1c491b9f781e548c
aidanakenes <aidanaknsm@gmail.com>
2022-02-26 13:34:36 +0600

    refact: column name from details to info

--- code/models.py
+++ code/models.py
@@ -2,23 +2,6 @@ from typing import List
 import json
 
 
-class Passenger:
-
-    def __init__(self, raw_data):
-        self.gender = raw_data.get('gender')
-        self.ticket_type = raw_data.get('ticket_type') if raw_data.get('ticket_type') else raw_data.get('type')
-        self.first_name = raw_data.get('first_name')
-        self.last_name = raw_data.get('last_name')
-        self.date_of_birth = raw_data.get('date_of_birth')
-        self.citizenship = raw_data.get('citizenship')
-        self.document_number = dict(raw_data.get('document')).get('number') if not raw_data.get(
-            'numbers') else raw_data.get('numbers')
-        self.document_expires_at = dict(raw_data.get('document')).get('expires_at') if not raw_data.get(
-            'expires_at') else raw_data.get('expires_at')
-        self.document_iin = dict(raw_data.get('document')).get('iin') if not raw_data.get('iin') else raw_data.get(
-            'iin')
-
-
 class Booking:
 
     def __init__(self, raw_data):
@@ -26,11 +9,8 @@ class Booking:
         self.phone = raw_data.get('phone')
         self.email = raw_data.get('email')
         self.offer = raw_data.get('offer') if raw_data.get('details') is None else json.loads(raw_data.get('details'))
-        self.passengers: List[Passenger]
-
-        if raw_data.get('passengers') is not None:
-            self.passengers = [Passenger(row) if type(row) is not Passenger else row for row in
-                               raw_data.get('passengers')]
+        self.passengers = raw_data.get('passenger') if raw_data.get('info') is None else json.loads(
+            raw_data.get('info'))
 
     def __dict__(self):
         return {
@@ -38,5 +18,5 @@ class Booking:
             'phone': self.phone,
             'email': self.email,
             'offer': self.offer,
-            'passengers': [p.__dict__ for p in self.passengers]
+            'passengers': self.passengers
         }
--- migrations/20220221102741_create_table_passenger.sql
+++ migrations/20220221102741_create_table_passenger.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS passenger (
     passenger_id SERIAL PRIMARY KEY,
-    details json NOT NULL
+    info json NOT NULL
 );
 
 -- migrate:down
