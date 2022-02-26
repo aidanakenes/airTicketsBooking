ad5c6bac2934881e2c0dae178567473334ad73c0
aidanakenes <aidanaknsm@gmail.com>
2022-02-26 13:43:57 +0600

    fix: booking selection from db

--- code/db.py
+++ code/db.py
@@ -19,7 +19,6 @@ async def create_booking(booking, *args, **kwargs):
 
 
 async def _insert_passenger(conn, passenger):
-
     stmt = """INSERT INTO passenger (info)
             VALUES ($1) RETURNING passenger_id;"""
 
@@ -29,7 +28,6 @@ async def _insert_passenger(conn, passenger):
 
 
 async def _insert_offer(conn, booking):
-
     stmt = """INSERT INTO offer_details (details)
             VALUES ($1) RETURNING offer_details_id;"""
 
@@ -39,7 +37,6 @@ async def _insert_offer(conn, booking):
 
 
 async def _insert_booking(conn, booking, passenger_id, offer_details_id):
-
     stmt = """INSERT INTO booking (booking_id, phone, email, offer_details_id, passenger_id)
             VALUES ($1, $2, $3, $4, $5) RETURNING booking_id;"""
 
@@ -55,54 +52,34 @@ async def get_booking(booking_id, *args, **kwargs):
 
     booking_data = await _select_booking(conn, booking_id)
 
-    booking_data.passengers = await _select_passengers(conn, booking_id)
-
     return booking_data
 
 
 async def _select_booking(conn, booking_id):
-    stmt = """SELECT b.offer_id, b.phone, b.email, od.details 
+    stmt = """SELECT b.booking_id, b.phone, b.email, od.details, p.info 
             FROM booking b
             INNER JOIN offer_details od ON b.offer_details_id=od.offer_details_id 
-            WHERE offer_id=$1"""
+            INNER JOIN passenger p ON b.passenger_id=p.passenger_id 
+            WHERE b.booking_id=$1"""
 
     booking_records = await conn.fetchrow(stmt, booking_id)
 
     return models.Booking(booking_records)
 
 
-async def _select_passengers(conn, booking_id):
-    stmt = """SELECT p.gender, p.ticket_type, p.first_name, p.last_name, p.date_of_birth, 
-            p.citizenship, p.numbers, p.expires_at, p.iin 
-            FROM passenger p 
-            INNER JOIN booking b ON p.passenger_id=b.passenger_id 
-            WHERE b.offer_id=$1"""
-
-    passenger_records = await conn.fetch(stmt, booking_id)
-
-    passengers = []
-    for p in passenger_records:
-        passengers.append(models.Passenger(p))
-
-    return passengers
-
-
 @with_connection
 async def get_bookings(email, phone, *args, **kwargs):
     conn = kwargs.pop('connection')
-    stmt = """SELECT b.offer_id, b.phone, b.email, od.details FROM booking b 
-        INNER JOIN offer_details od ON b.offer_details_id=od.offer_details_id 
-        WHERE b.email=$1 AND b.phone=$2;"""
+    stmt = """SELECT b.offer_id, b.phone, b.email, od.details, p.info 
+            FROM booking b 
+            INNER JOIN offer_details od ON b.offer_details_id=od.offer_details_id 
+            INNER JOIN passenger p ON b.passenger_id=p.passenger_id 
+            WHERE b.email=$1 AND b.phone=$2;"""
 
     rows = await conn.fetch(stmt, email, phone)
 
     bookings = []
     for row in rows:
-        passengers = await _select_passengers(conn, row.get('offer_id'))
-
-        booking_obj = models.Booking(row)
-        booking_obj.passengers = passengers
-
-        bookings.append(booking_obj)
+        bookings.append(models.Booking(row))
 
     return bookings
