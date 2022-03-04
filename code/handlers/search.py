fea28db5fe4e3b422fb42dbb7274dc13eb20887b
aidanakenes <aidanaknsm@gmail.com>
2022-03-04 22:13:42 +0600

    refact: aviata client

--- code/handlers/search.py
+++ code/handlers/search.py
@@ -7,17 +7,16 @@ import schemas
 import jsonschema_
 import cache
 from helpers.helpers import convert_currency
-from clients.aviata import AviataClient
+from clients.aviata import Client
 from helpers import errors
 
 
 async def search(request):
-    await jsonschema_.validate(request, schemas.SEARCH_SCHEMA)
-
+    await jsonschema_.validate(request.json, schemas.SEARCH_SCHEMA)
     if request.json.get('adults') < 1 and request.json.get('children') < 1 and request.json.get('infants') < 1:
         raise errors.PassengersNumberError()
 
-    provider_response = await AviataClient().search(request.json)
+    provider_response = await Client(base_url='https://avia-api.k8s-test.aviata.team').search(**json.loads(request.body))
     search_results = {
         'search_id': provider_response.json().get('search_id')
     }
