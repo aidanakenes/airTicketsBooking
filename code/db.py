370bf0c8590743288109268369849182146ceb9e
aidanakenes <aidanaknsm@gmail.com>
2022-03-04 11:58:17 +0600

    refact: remove methods for passenger table

--- code/db.py
+++ code/db.py
@@ -9,20 +9,10 @@ import models
 async def create_booking(booking, *args, **kwargs):
     conn = kwargs.pop('connection')
 
-    passenger_id = await _insert_passenger(conn, booking.passengers)
     offer_details_id = await _insert_offer(conn, booking.offer)
-    booking_id = await _insert_booking(conn, booking, passenger_id, offer_details_id)
+    booking_id = await _insert_booking(conn, booking, offer_details_id)
 
     return booking_id
-
-
-async def _insert_passenger(conn, passenger):
-    stmt = """INSERT INTO passenger (info)
-            VALUES ($1) RETURNING passenger_id;"""
-
-    uid = await conn.fetchval(stmt, json.dumps(passenger))
-
-    return uid
 
 
 async def _insert_offer(conn, offer):
@@ -35,11 +25,11 @@ async def _insert_offer(conn, offer):
 
 
 async def _insert_booking(conn, booking, passenger_id, offer_details_id):
-    stmt = """INSERT INTO booking (booking_id, phone, email, created_at, offer_details_id, passenger_id)
+    stmt = """INSERT INTO booking (booking_id, phone, email, created_at, passengers, offer_details_id, passenger_id)
             VALUES ($1, $2, $3, $4, $5, $6) RETURNING booking_id;"""
 
     uid = await conn.execute(stmt, booking.booking_id, booking.phone, booking.email, datetime.now(),
-                             offer_details_id, passenger_id)
+                             json.dumps(booking.passengers), offer_details_id, passenger_id)
 
     return uid
 
@@ -54,10 +44,9 @@ async def get_booking(booking_id, *args, **kwargs):
 
 
 async def _select_booking(conn, booking_id):
-    stmt = """SELECT b.booking_id, b.phone, b.email, od.details, p.info 
+    stmt = """SELECT b.booking_id, b.phone, b.email, od.details, b.passengers 
             FROM booking b
             INNER JOIN offer_details od ON b.offer_details_id=od.offer_details_id 
-            INNER JOIN passenger p ON b.passenger_id=p.passenger_id 
             WHERE b.booking_id=$1"""
 
     booking_records = await conn.fetchrow(stmt, booking_id)
@@ -68,10 +57,9 @@ async def _select_booking(conn, booking_id):
 @with_connection
 async def get_bookings(email, phone, *args, **kwargs):
     conn = kwargs.pop('connection')
-    stmt = """SELECT b.offer_id, b.phone, b.email, od.details, p.info 
+    stmt = """SELECT b.offer_id, b.phone, b.email, od.details, b.passengers 
             FROM booking b 
             INNER JOIN offer_details od ON b.offer_details_id=od.offer_details_id 
-            INNER JOIN passenger p ON b.passenger_id=p.passenger_id 
             WHERE b.email=$1 AND b.phone=$2;"""
 
     rows = await conn.fetch(stmt, email, phone)
